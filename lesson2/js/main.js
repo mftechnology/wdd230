ce93ab98527addcb65f9faaf5aa612ec4a0ff85f
marcelo.sduarte <90726831+mftechnology@users.noreply.github.com>
2022-09-23 17:50:09 -0300

    link to home page

---
+++ lesson2/js/main.js
@@ -0,0 +1,10 @@
+
+// set current year in footer
+const currentDate = new Date();
+document.querySelector('#year').textContent = currentDate.getFullYear();
+
+// last modified
+
+const lastModif = new Date();
+document.querySelector('#lastmod').textContent = lastModif;
+

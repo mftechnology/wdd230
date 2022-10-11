4bfe0c4b61f50e6bc4b54f014b17df6e7103ab82
marcelo.sduarte <90726831+mftechnology@users.noreply.github.com>
2022-10-11 17:42:23 -0300

    ajust icon face

---
+++ DOM/main.js
@@ -0,0 +1,8 @@
+const input = document.getElementById('input')
+const button = document.getElementById('button')
+const list = document.getElementById('list')
+
+button.addEventListener('click', function() {
+
+}
+

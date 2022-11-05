ee6f162d00de460c85d85410f26aa944aa26d9ac
marcelo.sduarte <90726831+mftechnology@users.noreply.github.com>
2022-11-05 09:53:20 -0300

    hour

--- chamber/scripts/main.js
+++ chamber/scripts/main.js
@@ -20,16 +20,18 @@ document.querySelector('#timeHeader').textContent = currentDate.getFullYear();
 
 
 
-
-
 // last modified
+
 const dateHeader = document.querySelector('#timeHeader');
 
 const fulldateUK = new Intl.DateTimeFormat("en-UK", {dateStyle: "full"}).format(currentDate);
 
 dateHeader.innerHTML = `<em>${fulldateUK}</em>`;
 
-document.querySelector("#lastModified").textContent = `Last Modification: ${document.lastModified}`;
+document.querySelector("#lastModified").textContent = document.lastModified();
+
+
+
 
 //Banner ------------------------------------------------------------------------------
 let bannerDay = new Date().getDay();

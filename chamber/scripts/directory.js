149e55f514b88fbe32de78d0c1f9fcca6a05368e
marcelo.sduarte <90726831+mftechnology@users.noreply.github.com>
2022-11-12 11:21:23 -0300

    business

--- chamber/scripts/directory.js
+++ chamber/scripts/directory.js
@@ -1,43 +1,38 @@
-import myJson from './json/data.json' assert {type: 'json'};
+//import myJson from './json/data.json' assert {type: 'json'};
+const requestURL = 'https://mftechnology.github.io/wdd230/chamber/json/data.json';
 
-fetch(myJson)
-    .then(function(response) {
-        if (!response.ok) {
-            throw new Error('Network response was not OK');
-          }
-        return response.json();
-    })
-
-    .then( function(jsonObject){
-        console.table(jsonObject);
-        const prophets = jsonObject['prophets'];
-        prophets.forEach(displayBusiness);
-    });
-
+async function getCompanies(){
+    const response = await fetch(requestURL);
+    if (response.ok){
+        const data = await response.json();
+        data.companies.forEach(company => {displayBusiness(company)})
+       
+    }
+}
 
 function displayBusiness(company) {
     //create elements to add to the document
     let card = document.createElement('section');
-    let h2 = document.createElement('h2');
+    let h3 = document.createElement('h3');
     let portait = document.createElement('img');
-    let birthdate = document.createElement('span');
-    let deathdate = document.createElement('span');
+    let name = document.createElement('span');
+    
 
     // change the textContent property of the h2 element to contain the prophets full name
-    h2.textContent = company.name;
+    h3.textContent = company.name;
  
 
     // build the image attributes by using the setAttribute method for the src, and loading attribute value (fill in the blank with the appropriate variable)
-    portait.setAttribute('src', company.imageurl);
+    portait.setAttribute('src', company.img);
     portait.setAttribute('alt', `Portait of ${company.name}`);
     portait.setAttribute('loading', 'lazy');
 
     // Add/append the section card with the h2 element
-    card.appendChild(h2);
+    card.appendChild(h3);
 
     card.appendChild(portait);
-    card.appendChild(birthdate);
-    card.appendChild(deathdate);
+    card.appendChild(name);
+    
 
     // add/ append the existing html div with the cards class with the section card
    //cards.appendChild(card);

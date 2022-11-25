059b9dc16d9646ef16da54126ec939bc9bcadd05
marcelo.sduarte <90726831+mftechnology@users.noreply.github.com>
2022-11-25 09:55:20 -0300

    final adjusts

--- chamber/scripts/spots.js
+++ chamber/scripts/spots.js
@@ -28,31 +28,46 @@ function displayBusiness(company) {
     //create elements to add to the document
     let card = document.createElement('section');
     let portait = document.createElement('img');
-    let member = document.createElement('span');
-    let category = document.createElement('p');
+    let h3 = document.createElement('h3');
+    let hr = document.createElement('hr');
+    let phone = document.createElement('p');
+    let memberLevel = document.createElement('p');
+  
  
     // change the textContent property of the h2 element to contain the prophets full name
-    category.textContent = company.Category;
+    phone.textContent = `Phone: ${company.phone} `;
+    memberLevel.textContent = `Membership-level: ${company.memberLevel} `;
 
     // build the image attributes by using the setAttribute method for the src, and loading attribute value (fill in the blank with the appropriate variable)
     portait.setAttribute('src', company.img);
     portait.setAttribute('alt', `Portait of ${company.name}`);
     portait.setAttribute('loading', 'lazy');
+    portait.setAttribute('width', "50%")
   
  
     // Add/append the section card with the h2 element
+    card.appendChild(h3);
     card.appendChild(portait);
-    card.appendChild(member);
-    card.appendChild(category);
+    card.appendChild(hr);
+    card.appendChild(phone);
+    card.appendChild(memberLevel);
+  
     
-
-
     
     // add/ append the existing html div with the cards class with the section card
    //cards.appendChild(card);
-     console.log(`IMPRIME: ${company.Category}`);
-        //document.querySelector('div.stay').appendChild(card);    
- 
+    if (company.Category === "Stay") {
+        h3.textContent = "Where to Stay";
+        document.querySelector('div.Spotlight1').appendChild(card);    
+    } 
+    if (company.Category === "Food") {
+        h3.textContent = "Where to Eat";
+        document.querySelector('div.Spotlight2').appendChild(card);    
+    } 
+    if (company.Category === "Place") {
+        h3.textContent = "What To Do";
+        document.querySelector('div.Spotlight3').appendChild(card);    
+    } 
 }
 
 

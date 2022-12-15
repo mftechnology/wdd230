f8aceee0e09c58bcab007eefe7a767b270e89f51
marcelo.sduarte <90726831+mftechnology@users.noreply.github.com>
2022-12-15 10:36:54 -0300

    ajust btn home

--- finalproject/scripts/main.js
+++ finalproject/scripts/main.js
@@ -1,4 +1,16 @@
+//Menu Hamburger ------------------------------------------------------
+function toggleMenu(){
+    document.getElementById('navigation').classList.toggle("open");
+  }
+  
+  const x = document.getElementById('hambBtn')
+  x.onclick = toggleMenu;
+
+
+  
+
 const dateMod = new Date(document.lastModified);
 const formatData = new Intl.DateTimeFormat("en-GB", {dateStyle: "short"}).format(dateMod);
 document.getElementById("lastModified").innerHTML = formatData;
-  
+
+
